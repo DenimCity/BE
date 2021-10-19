5ef07d05c4218987dc1763d5dbdb20a5c7f2135b
Jean Altidor <altidorj1@gmail.com>
2021-10-19 11:39:37 -0400

    created service layer

---
+++ springboot/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,15 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    public static List<Student> getStudents() {
+        return List.of(new Student(1L, "Mariam", "jamal@email.com", LocalDate.of(2000, Month.JANUARY, 5), 21));
+    }
+}
